dee02e02385b4463ce894f429999ee5d7efb3b14
Bisheng Liu <bisheng@postnl.nl>
2021-05-01 17:56:46 +0200

    add test project

---
+++ warehouse-api/tests/Warehouse.ServiceTests/InventoryServiceTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Warehouse.ServiceTests
+{
+    public class InventoryServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ warehouse-api/tests/Warehouse.ServiceTests/ProductServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Warehouse.ServiceTests
+{
+    class ProductServiceTests
+    {
+    }
+}
