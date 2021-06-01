57438ded372b800d9c64893c586e93d06c712a4c
Bisheng Liu <bisheng@postnl.nl>
2021-06-01 19:21:00 +0200

    add working navbar

--- warehouse-react/src/components/navbar/navbar.tsx
+++ warehouse-react/src/components/navbar/navbar.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 import './navbar.css';
 
 const NavBar = () => 
-<div>
-    <Link to='/home'>Home</Link>
-    <Link to='/inventory'>Inventory</Link>
-    <Link to='/product'>Product</Link>
-</div>
+    <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand as={Link} to="/" className="brand-padding">Warehouse</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+                <NavDropdown title="Inventory" id="basic-nav-dropdown">
+                    <NavDropdown.Item as={Link} to="/inventory">All articles</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/inventory">Upload articles</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title="Product" id="basic-nav-dropdown">
+                    <NavDropdown.Item as={Link} to="/product">All products</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/product">Upload products</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link as={Link} to="/product">Stock</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
 
 export default NavBar;
--- warehouse-react/src/features/home/home.tsx
+++ warehouse-react/src/features/home/home.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 const Home = () => 
-<div>home page</div>
+<div>home page
+    <Button variant="primary">demo</Button>
+</div>
 
 
 export default Home;
