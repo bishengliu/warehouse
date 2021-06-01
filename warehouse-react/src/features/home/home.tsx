8bf3e8777a8b2f6dacb7f8e7d4283791512db593
Bisheng Liu <bisheng@postnl.nl>
2021-06-01 21:19:08 +0200

    tidy up some boilerplates

--- warehouse-react/src/features/home/home.tsx
+++ warehouse-react/src/features/home/home.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 
 const Home = () => 
-<div>home page
-    <Button variant="primary">demo</Button>
-</div>
+<div>home page</div>
 
 
 export default Home;
