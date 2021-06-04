d7e025393b1065f2ec9127de5d698969e8c93007
Bisheng Liu <bisheng@postnl.nl>
2021-06-04 11:44:24 +0200

    render the article component

--- warehouse-react/src/features/inventory/inventory.tsx
+++ warehouse-react/src/features/inventory/inventory.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Table, Spinner } from 'react-bootstrap';
 import AppState from '../../redux/states/app-state';
 import { getAllArticlesActionCreatorAsync } from '../../redux/actions/article-action-creators';
 
@@ -14,11 +15,67 @@ const Inventory = (): JSX.Element => {
     loadData();
     setIsLoaded(true);
   }
-  console.log(articleState.articles);
+
   return (
     <div>
-      inventory:
-      { articleState.articles.length }
+      {
+        !isLoaded && (
+          <div className="text-left p-3">
+            <Spinner animation="border" variant="secondary" />
+            <span> loading...</span>
+          </div>
+
+        )
+      }
+
+      {
+        articleState.articles.length === 0 && isLoaded
+        && (
+          <div>No article found!</div>
+        )
+      }
+
+      { articleState.articles.length > 0 && isLoaded
+        && (
+          <Table responsive striped bordered hover variant="light">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Stock</th>
+                <th>Description</th>
+                <th>actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                articleState.articles.map((article) => (
+                  <tr>
+                    <td>
+                      { article.id }
+                    </td>
+                    <td>
+                      { article.name }
+                    </td>
+                    <td>
+                      { article.price }
+                    </td>
+                    <td>
+                      { article.stock }
+                    </td>
+                    <td>
+                      { article.description }
+                    </td>
+                    <td>
+                      <span>action</span>
+                    </td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </Table>
+        )}
     </div>
   );
 };
